c6ea73cfca1678aa0547643b66b9a425525d7de0
Jiahuan <zjsyhjh@gmail.com>
2016-12-07 12:56:52 +0800

    add Add function

--- src/lru/lru.go
+++ src/lru/lru.go
@@ -12,13 +12,14 @@ type entry struct {
 }
 
 /*
- * ll用来保存所有的item, 采用lru缓存算法, maxEntries指的是最大缓存数量
+ * ll用来保存所有的item, 采用lru缓存算法, maxEntries指的是最大缓存数量, 0默认是不限制数量
  * list.Element指向的类型为entry类型
  */
 type Cache struct {
 	ll         *list.List
 	cache      map[interface{}]*list.Element
 	maxEntries int
+	Evict      func(key Key, value interface{})
 }
 
 /*
@@ -29,5 +30,25 @@ func NewCache(maxEntries int) *Cache {
 		ll:         list.New(),
 		cache:      make(map[interface{}]*list.Element),
 		maxEntries: maxEntries,
+	}
+}
+
+/*
+ * add value to Cache
+ */
+func (c *Cache) Add(key Key, value interface{}) {
+	if c.cache == nil {
+		c.ll = list.New()
+		c.cache = make(map[interface{}]*list.Element)
+	}
+
+	if e, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(e)
+		e.Value.(*entry).value = value
+		return
+	}
+	e := c.ll.PushFront(&entry{key, value})
+	c.cache[key] = e
+	if c.maxEntries != 0 && c.ll.Len() > c.maxEntries {
 	}
 }
