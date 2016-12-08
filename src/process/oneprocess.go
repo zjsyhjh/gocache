2286b2eebeb04c837804e0810eaa92418ac5f0d7
Jiahuan <zjsyhjh@gmail.com>
2016-12-08 16:28:13 +0800

    add comment

--- src/process/oneprocess.go
+++ src/process/oneprocess.go
@@ -19,6 +19,11 @@ type Group struct {
 	mapCall map[string]*call
 }
 
+/*
+ * DoProcess处理时间间隔很多的相同的多次请求，合并处理，返回结果，这样能够减轻系统压力
+ * 将每次的请求key进行缓存，如果很短时间内有多个相同key的请求到来，则等待，只处理第一次的
+ * 其余的直接依据第一次的结果返回
+ */
 func (g *Group) DoProcess(key string, fn func() (interface{}, error)) (interface{}, error) {
 	g.mu.Lock()
 
@@ -39,9 +44,11 @@ func (g *Group) DoProcess(key string, fn func() (interface{}, error)) (interface
 
 	g.mu.Unlock()
 
+	//处理完请求之后，执行sync.WaitGroup.Done()，这样别的key相同的请求就能直接放回结果
 	c.value, c.err = fn()
 	c.wg.Done()
 
+	//处理完后删除本次请求的key
 	g.mu.Lock()
 	delete(g.mapCall, key)
 	g.mu.Unlock()
