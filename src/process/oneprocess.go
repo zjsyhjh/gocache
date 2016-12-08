298fa803e5b0712ccd7722e1577e81440d12d3ea
Jiahuan <zjsyhjh@gmail.com>
2016-12-08 13:08:29 +0800

    add DoProcess function

--- src/process/oneprocess.go
+++ src/process/oneprocess.go
@@ -1,8 +1,6 @@
 package process
 
-import (
-	"sync"
-)
+import "sync"
 
 /*
  * 每个请求
@@ -19,4 +17,34 @@ type call struct {
 type Group struct {
 	mu      sync.Mutex
 	mapCall map[string]*call
+}
+
+func (g *Group) DoProcess(key string, fn func() (interface{}, error)) (interface{}, error) {
+	g.mu.Lock()
+
+	if g.mapCall == nil {
+		g.mapCall = make(map[string]*call)
+	}
+
+	//已经有请求在处理了，则等待
+	if c, ok := g.mapCall[key]; ok {
+		g.mu.Unlock()
+		c.wg.Wait()
+		return c.value, c.err
+	}
+
+	c := new(call)
+	c.wg.Add(1)
+	g.mapCall[key] = c
+
+	g.mu.Unlock()
+
+	c.value, c.err = fn()
+	c.wg.Done()
+
+	g.mu.Lock()
+	delete(g.mapCall, key)
+	g.mu.Unlock()
+
+	return c.value, c.err
 }
