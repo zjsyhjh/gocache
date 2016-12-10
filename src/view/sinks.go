7ded53c1edb6e6fc65c46ad66dbdefdcaff1bbe8
Jiahuan <zjsyhjh@gmail.com>
2016-12-10 15:15:34 +0800

    add Sink interface

--- src/view/sinks.go
+++ src/view/sinks.go
@@ -1,5 +1,19 @@
 package view
 
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+/*
+ * A Sink receive data from a Get call
+ */
+type Sink interface {
+	SetBytes(bytes []byte) error
+	SetString(s string) error
+	SetProto(m proto.Message) error
+	view() (ByteView, error)
+}
+
 func cloneBytes(b []byte) []byte {
 	c := make([]byte, len(b))
 	copy(c, b)
