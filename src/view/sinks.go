50c1722dcd2bb34f4649076f6e2f72a1414ef0ef
Jiahuan <zjsyhjh@gmail.com>
2016-12-11 15:45:33 +0800

    move stringSink to stringsink.go

--- src/view/sinks.go
+++ src/view/sinks.go
@@ -12,42 +12,6 @@ type Sink interface {
 	view() (ByteView, error)
 }
 
-type stringSink struct {
-	strptr   *string
-	byteview ByteView
-}
-
-/*
- * stringSink implement all functions of Sink interface
- */
-func StringSink(strptr *string) Sink {
-	return &stringSink{strptr: strptr}
-}
-
-func (s *stringSink) SetBytes(bytes []byte) error {
-	return s.SetString(string(bytes))
-}
-
-func (s *stringSink) SetString(str string) error {
-	s.byteview.bytes = nil
-	s.byteview.str = str
-	*s.strptr = str
-	return nil
-}
-
-func (s *stringSink) SetProto(m proto.Message) error {
-	bytes, err := proto.Marshal(m)
-	if err != nil {
-		return err
-	}
-	s.byteview.bytes = bytes
-	*s.strptr = string(bytes)
-	return nil
-}
-
-func (s *stringSink) view() (ByteView, error) {
-	return s.byteview, nil
-}
 
 func cloneBytes(b []byte) []byte {
 	c := make([]byte, len(b))
