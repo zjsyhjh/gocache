3015c4e96ce9e33908ca89790735cb4de59ebecf
Jiahuan <zjsyhjh@gmail.com>
2016-12-11 15:26:05 +0800

    stringSink implemnt the functions of Sink interface

--- src/view/sinks.go
+++ src/view/sinks.go
@@ -1,15 +1,13 @@
 package view
 
-import (
-	"github.com/golang/protobuf/proto"
-)
+import "github.com/golang/protobuf/proto"
 
 /*
  * A Sink receive data from a Get call
  */
 type Sink interface {
 	SetBytes(bytes []byte) error
-	SetString(s string) error
+	SetString(str string) error
 	SetProto(m proto.Message) error
 	view() (ByteView, error)
 }
@@ -17,6 +15,38 @@ type Sink interface {
 type stringSink struct {
 	strptr   *string
 	byteview ByteView
+}
+
+/*
+ * stringSink implement all functions of Sink interface
+ */
+func StringSink(strptr *string) Sink {
+	return &stringSink{strptr: strptr}
+}
+
+func (s *stringSink) SetBytes(bytes []byte) error {
+	return s.SetString(string(bytes))
+}
+
+func (s *stringSink) SetString(str string) error {
+	s.byteview.bytes = nil
+	s.byteview.str = str
+	*s.strptr = str
+	return nil
+}
+
+func (s *stringSink) SetProto(m proto.Message) error {
+	bytes, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+	s.byteview.bytes = bytes
+	*s.strptr = string(bytes)
+	return nil
+}
+
+func (s *stringSink) view() (ByteView, error) {
+	return s.byteview, nil
 }
 
 func cloneBytes(b []byte) []byte {
