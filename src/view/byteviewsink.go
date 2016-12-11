41c1a9c955c4878e0a630d9c24e3f88b8895d6d0
Jiahuan <zjsyhjh@gmail.com>
2016-12-11 15:58:17 +0800

    implement SetBytes, SetString, SetProto, view and setView functions

--- src/view/byteviewsink.go
+++ src/view/byteviewsink.go
@@ -17,17 +17,29 @@ func ByteViewSink(dst *ByteView) Sink {
 }
 
 func (s *byteViewSink) SetBytes(bytes []byte) error {
-
+	*s.dst = ByteView{bytes: cloneBytes(bytes)}
+	return nil
 }
 
 func (s *byteViewSink) SetString(str string) error {
-
+	*s.dst = ByteView{str: str}
+	return nil
 }
 
 func (s *byteViewSink) SetProto(m proto.Message) error {
-
+	bytes, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+	*s.dst = ByteView{bytes: bytes}
+	return nil
 }
 
 func (s *byteViewSink) view() (ByteView, error) {
+	return *s.dst, nil
+}
 
+func (s *byteViewSink) setView(view ByteView) error {
+	*s.dst = view
+	return nil
 }
